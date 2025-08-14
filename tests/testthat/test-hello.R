952f35615f884964bca56f6eed6383dee29a60f5
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-08-13 18:59:27 -0700

    Add GitHub links to DESCRIPTION

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,8 +1,3 @@
-test_that("works on Windows", {
-  skip_on_os(c("mac", "linux"))
-  hello() |> expect_snapshot()
-})
-
 test_that("works on Mac", {
   skip_on_os(c("windows", "linux"))
   hello() |> expect_snapshot()
